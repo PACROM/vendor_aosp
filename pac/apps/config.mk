386fbd6b4f61972f1e83f01c1dba3e9167cd5abf
ShoreDroid <53381903+PACROM@users.noreply.github.com>
2020-07-16 09:18:45 -0400

    Update config.mk

--- pac/apps/config.mk
+++ pac/apps/config.mk
@@ -2,9 +2,7 @@
 # PACROM Apps Inclusion Makefile
 #
 
-PAC_APPS := vendor/pac/apps
-
-# app    
+# system apps    
 PRODUCT_PACKAGES += \
     3627fd50-894f-465d-ab5a-8a7334b3d59d.apk \
     ota-updates
